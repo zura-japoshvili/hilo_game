5b93a79d2b468aed87baea3e27e7e90e60f5fc8f
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-12-26 16:03:45 +0400

    update

--- js/script.js
+++ js/script.js
@@ -7,35 +7,18 @@ let bottomCont = $(".bottom-cont"),
 
 let gameActive = false;
 
+let cardsBtn;
 
 
-// let za = $(".cards-btn");
 // za.click(function () { 
 //     console.log(za[0]);
 //     $(za[0]).css("transform", "rotateY(180deg)");
 // });
 
 
-
-
-
-
-betBtn.click(startFunc);
-function startFunc(){
-    if(!gameActive){
-        topCont.css("top", "40px");
-        topImg.css({"width": "100px", "height": "160px"});
-        cardList.css({"width": "80px", "height": "120px"});
-        setTimeout(() =>{
-            topCont.css("position", "static");
-            generateCards();
-        }, 400);
-    }
-    generateCards()
+function cardClickHandler(clickedIndex){
+    console.log(clickedIndex);
 }
-
-
-
 
 function generateCards() {
     bottomCont.html(`<div class="cards">
@@ -74,5 +57,27 @@ function generateCards() {
             <p>Win. <span>1.12$</span>$</p>
         </div>
     </div`);
+
+    cardsBtn = $(".cards-btn");
+    cardsBtn.each(function (indexInArray, valueOfElement) { 
+        $(valueOfElement).click(function () {
+            cardClickHandler(indexInArray);
+        });
+    });
+}
+
+
+betBtn.click(startFunc);
+function startFunc(){
+    if(!gameActive){
+        topCont.css("top", "40px");
+        topImg.css({"width": "100px", "height": "160px"});
+        cardList.css({"width": "80px", "height": "120px"});
+        setTimeout(() =>{
+            topCont.css("position", "static");
+            setTimeout(generateCards, 50);
+        }, 400);  
+    }
+    
 }
 
